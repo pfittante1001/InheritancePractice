5a9e1016b110fe943f0ee394a5db6c4a1c293c66
pfittante <pfittante@yahoo.com>
2017-07-13 13:21:24 -0400

    Create Automobile class

---
+++ InheritancePractice/Automobile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//// Child Class
+//Inhetits from Parent class = Vehicle
+namespace InheritancePractice
+{
+    class Automobile    : Vehicle
+    {
+        //fields
+        private int numWheels;
+        private int numDoors;
+        private float wheelSize;
+
+        //properties
+
+        public int NumWheels { get; }
+        public int NumDoors { get; }
+        public float WheelSize { get; }
+
+        //Constructor
+
+        public Automobile()
+        {
+            //default constructor
+        }
+
+        public Automobile(int numWheels, float wheelSize, int numDoors, int seats, 
+            int carryingCapacity, string color, double movementSpeed )
+        {
+            this.numDoors = numWheels;
+            this.numWheels = numWheels;
+            this.wheelSize = wheelSize;
+            this.seats = seats;
+            this.carryingCapacity = carryingCapacity;
+            this.color = color;
+            this.movementSpeed = movementSpeed;
+        }
+
+
+    }
+}
--- InheritancePractice/Program.cs
+++ InheritancePractice/Program.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//Parent class
-//Children = vehicle
 
 namespace InheritancePractice
 {
@@ -12,10 +10,16 @@ namespace InheritancePractice
     {
         static void Main(string[] args)
         {
-            Boat firstBoat = new Boat(2, 500, "red", 70d, .78d);
-            firstBoat.Move();
-            firstBoat.Move();
-            Console.WriteLine("Boat distance traveled {0} :", firstBoat.GetDistanceTraveled());
+            //Boat firstBoat = new Boat(2, 500, "red", 70d, .78d);
+            //firstBoat.Move();
+            //firstBoat.Move();
+            //Console.WriteLine("Boat distance traveled {0} :", firstBoat.GetDistanceTraveled());
+
+            Automobile firstAuto = new Automobile(4, 20, 4, 5, 500, "red", 260d);
+            firstAuto.Move();
+            firstAuto.Color = "silver";
+            Console.WriteLine("Automobile {0} :", firstAuto.GetDistanceTraveled());
+
         }
     }
 }
