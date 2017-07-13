d18eacf31f3e6448b44ffd2c108f4437689bc051
pfittante <pfittante@yahoo.com>
2017-07-13 10:30:00 -0400

    Create Vehicle class

--- InheritancePractice/Vehicle.cs
+++ InheritancePractice/Vehicle.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Child class
+//Parent class == Program
 
 namespace InheritancePractice
 {
     class Vehicle
     {
+
+        //fields
+        protected int seats;
+        protected int carryingCapacity;
+        protected string color;
+        protected double movementSpeed;
+        protected double distanceTraveled;
+        private float gearRatio;
+        private float engineSize;
+
+        //properties
+
+        public int Seats { get; }
+        public int CarryingCapacity { get; }
+        public string Color { get; set; }
+        public float GearRatio { get; set; }
+        public float EngineSize { get; set; }
+        public double DistanceTraveled { get; set; }
+
+        //constructors
+
+        public Vehicle()
+        {
+            //default constructor
+        }
+
+        public Vehicle(int seats, int carryingCapacity, string color, double movementSpeed)
+        {
+            this.seats = seats;
+            this.carryingCapacity = carryingCapacity;
+            this.color = color;
+            this.movementSpeed = movementSpeed;
+
+            distanceTraveled = 0;
+        }
+
+        public virtual void Move()
+        {
+            distanceTraveled += movementSpeed;
+        }
+
+        public virtual string GetDistanceTraveled()
+        {
+            return "Distance Traveled" + distanceTraveled + "\n";
+        }
     }
 }
